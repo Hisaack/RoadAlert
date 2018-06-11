8113d3ae13ac56714727120ac4d71634f0d9a2bf
Jcardif <josh.cardif14@outlook.com>
2018-06-11 10:35:10 +0300

    Add UWP app

--- RoadAlertUWP/App.xaml.cs
+++ RoadAlertUWP/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace RoadAlert
+namespace RoadAlertUWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- RoadAlertUWP/Properties/AssemblyInfo.cs
+++ RoadAlertUWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RoadAlert")]
+[assembly: AssemblyTitle("RoadAlertUWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RoadAlert")]
+[assembly: AssemblyProduct("RoadAlertUWP")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
