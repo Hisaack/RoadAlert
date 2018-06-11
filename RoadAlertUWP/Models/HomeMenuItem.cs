39a0b48d5e3379d5154841679c4c8c8f7908e517
Jcardif <josh.cardif14@outlook.com>
2018-06-11 11:13:55 +0300

    Add Assets

---
+++ RoadAlertUWP/Models/HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadAlertUWP.Models
+{
+    public class HomeMenuItem
+    {
+        public string ItemName { get; set; }
+        public string ItemLocation { get; set; }
+    }
+
+    public class ManageMenuItem
+    {
+        
+    }
+}
---
+++ RoadAlertUWP/Pages/RoadAlertHome.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RoadAlertUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RoadAlertHome : Page
+    {
+        public RoadAlertHome()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchRoadBox_OnQuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HomeMenuListView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
