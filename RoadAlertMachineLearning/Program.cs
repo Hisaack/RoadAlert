f94fa5dd29d5730e453368ba775520ed67b3cfbd
Jcardif <josh.cardif14@outlook.com>
2018-06-13 16:11:57 +0300

    Add ML Project

---
+++ RoadAlertMachineLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoadAlertMachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
